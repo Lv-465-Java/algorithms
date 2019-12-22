dc6162adaf8aff14044fd7134750af132bd043c8
sydor_artur <sydor.artur@ukr.net>
2019-12-23 00:31:43 +0200

    add method sumOfEndNumbers

--- src/main/java/lv465/algoritm/chapter/Chapter1.java
+++ src/main/java/lv465/algoritm/chapter/Chapter1.java
@@ -1,4 +1,19 @@
 package lv465.algoritm.chapter;
 
+import java.util.Arrays;
+
 public class Chapter1 {
+
+    public Integer sumOfEndNumbers(int n, int m) {
+        String number = new StringBuilder("" + n).reverse().toString();
+        if (number.length() < m) {
+            return 0;
+        }
+        Integer sumOfEnd = Arrays.stream(number.split(""))
+                .filter(elem -> !elem.equals(""))
+                .mapToInt(Integer::parseInt)
+                .limit(m)
+                .sum();
+        return sumOfEnd;
+    }
 }
