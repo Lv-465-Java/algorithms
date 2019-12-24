cb824f69d30e6bd938ccb25328a087bcfc5b84bc
sydor_artur <sydor.artur@ukr.net>
2019-12-24 15:11:25 +0200

    add method findMertensNumbers and util methods isPrime, isMertens

--- src/main/java/com/lv465/chapters/Chapter2.java
+++ src/main/java/com/lv465/chapters/Chapter2.java
@@ -22,4 +22,41 @@ public class Chapter2 {
         }
         return multiples;
     }
+
+    public List<Integer> findMertensNumbers(int n) {
+        List<Integer> mertonsList = new ArrayList<>();
+        for (int i = n; i > 0; i--) {
+            if (isPrime(i)) {
+                if (isMertens(i)) {
+                    mertonsList.add(i);
+                }
+            }
+        }
+        return mertonsList;
+    }
+
+    private boolean isPrime(int number) {
+        for (int i = 2; i < Math.sqrt(number); i++) {
+            if (number % 2 == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean isMertens(int number) {
+        int mertensForm = 0;
+        int power = 1;
+        while (mertensForm <= number) {
+            if (isPrime(power)) {
+                mertensForm = (int) Math.pow(2, power);
+                mertensForm -= 1;
+                if (mertensForm == number) {
+                    return true;
+                }
+            }
+            power++;
+        }
+        return false;
+    }
 }
