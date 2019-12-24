7f42b8f04074d05a4ec280dfe4b837675afa8e98
sydor_artur <sydor.artur@ukr.net>
2019-12-24 15:07:35 +0200

    add method sumOfEndNumbers

--- src/main/java/com/lv465/chapters/Chapter1.java
+++ src/main/java/com/lv465/chapters/Chapter1.java
@@ -1,4 +1,20 @@
 package com.lv465.chapters;
 
+import java.util.Arrays;
+
 public class Chapter1 {
+
+    public Integer sumOfEndNumbers(int n, int m) {
+        String number = new StringBuilder("" + n).reverse().toString();
+        if (number.length() < m) {
+            return 0;
+        }
+        Integer sumOfEnd = Arrays.stream(number.split(""))
+                .filter(elem -> !elem.equals(""))
+                .mapToInt(Integer::parseInt)
+                .limit(m)
+                .sum();
+        return sumOfEnd;
+    }
+
 }
