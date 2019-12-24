b45d0cfc2066d2bda155ceb624c5c4c0cdc20ca5
maryshchur <marichka02700@gmail.com>
2019-12-24 17:20:10 +0200

    Added squereOfEvenNumber and multipleOn3ButNot5 methods to Chapter1 and pythagoreanTriplets method to Chapter2

--- src/main/java/com/lv465/chapters/Chapter1.java
+++ src/main/java/com/lv465/chapters/Chapter1.java
@@ -1,4 +1,35 @@
 package com.lv465.chapters;
 
+import java.util.stream.IntStream;
+
 public class Chapter1 {
+    /**
+     * Method for returning amount of natural numbers
+     * that are square of even numbers
+     *
+     * @param max_number number is read by Scanner
+     * @return long value - amount of numbers
+     * @author Maria Shchur
+     */
+    public long squareOfEvenNumber(int max_number){
+        return IntStream.
+                rangeClosed(1,max_number).
+                filter(q->(Math.sqrt(q)%2==0)).
+                count();
+    }
+
+    /**
+     * This method return amount of natural numbers
+     * multiple on 3 but not 5
+     *
+     * @param max_number number is read by Scanner
+     * @return long value - amount of numbers
+     * @author Maria Shchur
+     */
+    public long multipleOn3ButNot5(int max_number){
+        return IntStream.
+                rangeClosed(1,max_number).
+                filter(q->(q%3==0 && q%5!=0)).
+                count();
+    }
 }
