7dfc2df47530ae9357cfb71c6eff3016d8e0a452
MarianDutchyn <marian.dutchyn@ukr.net>
2019-12-26 15:50:40 +0200

    change javdoc for method getMaximumNumber

--- src/main/java/com/lv465/chapter/Chapter1.java
+++ src/main/java/com/lv465/chapter/Chapter1.java
@@ -266,8 +266,7 @@ public class Chapter1 {
     }
 
     /**
-     *   This method returns the maximum integer k
-     *   in which 4^k < n.
+     *   This method returns the maximum integer k in which 4^k < n.
      *   @author Marian Dutchyn
      *   @param n an integer > 1
      *   @returns maximum integer in which 4^k < n
